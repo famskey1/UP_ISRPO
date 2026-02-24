6f26e98d458938d20b3bdca6653f51c397c2d086
famskey1 <veraschvezova@gmail.com>
2026-02-24 21:00:38 +0500

    блок-схема и ерд-диаграмма

--- Demka_var1/Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Demka_var1/Backend/Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd6bcfa8eb41ce282c9153543a871c1c3ad3b287")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b030fd30af407a0488d28e69983c6ee1c98a5f51")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
